6cebde930d357a46fe4b5524007f196f7a67f9cf
Mikaaa <kevinle38@gmail.com>
2021-06-22 22:30:31 +0700

    update StaticWIndow

--- coffeeX/View/StaticWindow.xaml.cs
+++ coffeeX/View/StaticWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,43 @@ namespace coffeeX.View
     /// <summary>
     /// Interaction logic for StaticWindow.xaml
     /// </summary>
+
+    
     public partial class StaticWindow : Window
     {
+
+
+
+        List<string> month = new List<string>();
+        
+
         public StaticWindow()
         {
+            DateTime time = DateTime.Today;
+
+            List<int> year;
+            List<int> month = new List<int>() { 1,2,3,4,5,6,7,8,9,10,11,12};
+
             InitializeComponent();
+            
+            year = Enumerable.Range(2021, 11).ToList();
+            cmbYear.ItemsSource = year;
+            cmbYear.SelectedItem = time.Year;
+
+            cmbMonth.ItemsSource = month;
+            cmbMonth.SelectedItem = DateTime.Today.Month;
+
+            int tempyear =Convert.ToInt32(cmbYear.Text);
+            int teammonth = Convert.ToInt32(cmbMonth.Text);
+
+            List<int> day = Enumerable.Range(1, DateTime.DaysInMonth(tempyear, teammonth)).ToList();
+
+            cmbDay.ItemsSource = day;
+            cmbDay.SelectedItem = DateTime.Today.Day;
+
+
+
+
         }
 
        
